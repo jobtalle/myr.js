69c1a51e8df1cd87d6e00b3b91e5f1b4f6d91ab5
jobtalle <jobtalle@hotmail.com>
2018-02-09 23:55:48 +0100

    Converted to closure

--- js/myr.js
+++ js/myr.js
@@ -1,133 +1,131 @@
-var myr = {
-    initialize: function(canvasElement) {
-        this._gl = canvasElement.getContext("webgl");
-        this._clearColor = new myr.Color(0, 0, 0, 0);
-        this._width = canvasElement.width;
-        this._height = canvasElement.height;
+var myr = new function() {
+    var gl;
+    var clearColor;
+    var width, height;
+    var surface;
+    
+    var Color = this.Color = function(r, g, b, a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+
+        if(a === undefined)
+            this.a = 1;
+        else
+            this.a = a;
+    };
+    
+    var Vector = this.Vector = function(x, y) {
+        this.x = x;
+        this.y = y;
+    };
+    
+    Vector.prototype.add = function(vector) {
+        return new Vector(this.x + vector.x, this.y + vector.y);
+    };
+
+    Vector.prototype.subtract = function(vector) {
+        return new Vector(this.x - vector.x, this.y - vector.y);
+    };
+
+    Vector.prototype.negate = function() {
+        return new Vector(-this.x, -this.y);
+    };
+
+    Vector.prototype.dot = function(vector) {
+        return this.x * vector.x + this.y * vector.y;
+    };
+
+    Vector.prototype.length = function() {
+        return Math.sqrt(this.dot(this));
+    };
+
+    Vector.prototype.multiply = function(scalar) {
+        return new Vector(this.x * scalar, this.y * scalar);
+    };
+
+    Vector.prototype.divide = function(scalar) {
+        if(scalar === 0)
+            return new Vector(0, 0);
+        else
+            return this.multiply(1 / scalar);
+    };
+
+    Vector.prototype.normalize = function() {
+        return this.divide(this.length());
+    };
+    
+    Vector.prototype.angle = function() {
+        return Math.atan2(this.y, this.x);
+    };
+    
+    this.Surface = function(width, height) {
+        var clearColor = new Color(0, 0, 0, 0);
+        var texture = gl.createTexture();
+        var framebuffer = gl.createFramebuffer();
         
-        this.bind();
-    },
-    
-    setClearColor: function(color) {
-        this._clearColor = color;
-    },
-    
-    clear: function() {
-        this._gl.clearColor(this._clearColor.r, this._clearColor.g, this._clearColor.b, this._clearColor.a);
-        this._gl.clear(this._gl.COLOR_BUFFER_BIT);
-    },
-    
-    bind: function() {
-        this._surface = null;
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
         
-        this._gl.bindFramebuffer(this._gl.FRAMEBUFFER, null);
-        this._gl.viewport(0, 0, this._width, this._height);
-    }
-}
-
-myr.Surface = function(width, height) {
-    this._clearColor = new myr.Color(0, 0, 0, 0);
-    this._width = width;
-    this._height = height;
-    this.texture = myr._gl.createTexture();
-    this.framebuffer = myr._gl.createFramebuffer();
+        gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+        
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        
+        this.free = function() {
+            gl.deleteTexture(texture);
+            gl.deleteFramebuffer(framebuffer);
+        };
+        
+        this.getWidth = function() {
+            return width;
+        };
+        
+        this.getHeight = function() {
+            return height;
+        };
+        
+        this.setClearColor = function(color) {
+            clearColor = color;
+        };
+        
+        this.bind = function() {
+            surface = this;
+            
+            gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+            gl.viewport(0, 0, width, height);
+        };
+        
+        this.clear = function() {
+            gl.clearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+        };
+    };
     
-    myr._gl.bindTexture(myr._gl.TEXTURE_2D, this.texture);
-    myr._gl.texParameteri(myr._gl.TEXTURE_2D, myr._gl.TEXTURE_MAG_FILTER, myr._gl.NEAREST);
-    myr._gl.texParameteri(myr._gl.TEXTURE_2D, myr._gl.TEXTURE_MIN_FILTER, myr._gl.LINEAR);
-    myr._gl.texImage2D(myr._gl.TEXTURE_2D, 0, myr._gl.RGBA, width, height, 0, myr._gl.RGBA, myr._gl.UNSIGNED_BYTE, null);
+    var bind = this.bind = function() {
+        surface = null;
+        
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.viewport(0, 0, width, height);
+    };
     
-    myr._gl.bindFramebuffer(myr._gl.FRAMEBUFFER, this.framebuffer);
-    myr._gl.framebufferTexture2D(
-        myr._gl.FRAMEBUFFER,
-        myr._gl.COLOR_ATTACHMENT0,
-        myr._gl.TEXTURE_2D,
-        this.texture,
-        0);
+    this.initialize = function(canvasElement) {
+        gl = canvasElement.getContext("webgl");
+        clearColor = new Color(0, 0, 0, 0);
+        width = canvasElement.width;
+        height = canvasElement.height;
+        
+        bind();
+    };
     
-    myr._gl.bindFramebuffer(myr._gl.FRAMEBUFFER, null);
-}
-
-myr.Surface.prototype.free = function() {
-    myr._gl.deleteTexture(this.texture);
-    myr._gl.deleteFramebuffer(this.framebuffer);
-}
-
-myr.Surface.prototype.getWidth = function() {
-    return this._width;
-}
-
-myr.Surface.prototype.getHeight = function() {
-    return this._height;
-}
-
-myr.Surface.prototype.setClearColor = function(color) {
-    this._clearColor = color;
-}
-
-myr.Surface.prototype.clear = function() {
-    myr._gl.clearColor(this._clearColor.r, this._clearColor.g, this._clearColor.b, this._clearColor.a);
-    myr._gl.clear(myr._gl.COLOR_BUFFER_BIT);
-}
-
-myr.Surface.prototype.bind = function() {
-    myr._surface = this;
+    this.setClearColor = function(color) {
+        clearColor = color;
+    };
     
-    myr._gl.bindFramebuffer(myr._gl.FRAMEBUFFER, this.fbo);
-    myr._gl.viewport(0, 0, this.width, this.height);
-}
-
-myr.Color = function(r, g, b, a) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-
-    if(a == undefined)
-        this.a = 1;
-    else
-        this.a = a;
-}
-
-myr.Vector = function(x, y) {
-	this.x = x;
-	this.y = y;
-}
-
-myr.Vector.prototype.add = function(vector) {
-    return new myr.Vector(this.x + vector.x, this.y + vector.y);
-}
-
-myr.Vector.prototype.subtract = function(vector) {
-    return new myr.Vector(this.x - vector.x, this.y - vector.y);
-}
-
-myr.Vector.prototype.negate = function() {
-    return new myr.Vector(-this.x, -this.y);
-}
-
-myr.Vector.prototype.dot = function(vector) {
-    return this.x * vector.x + this.y * vector.y;
-}
-
-myr.Vector.prototype.length = function() {
-    return Math.sqrt(this.dot(this));
-}
-
-myr.Vector.prototype.multiply = function(scalar) {
-    return new myr.Vector(this.x * scalar, this.y * scalar);
-}
-
-myr.Vector.prototype.divide = function(scalar) {
-    if(scalar == 0)
-        return new myr.Vector(0, 0);
-    else
-        return this.multiply(1 / scalar);
-}
-
-myr.Vector.prototype.normalize = function() {
-    return this.divide(this.length());
-}
-
-myr.Vector.prototype.angle = function() {
-    return Math.atan2(this.y, this.x);
-}
+    this.clear = function() {
+        gl.clearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    };
+};
