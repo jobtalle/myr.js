99d4384e8f42da1267e5d4a35943cb595a66100d
Job <jobtalle@hotmail.com>
2018-05-04 15:57:46 +0200

    Isometric renderer keeps matrices global

--- examples/isometric/js/isometric.js
+++ examples/isometric/js/isometric.js
@@ -8,24 +8,50 @@ const Isometric = function(myr) {
         const texture = textures[Math.floor(Math.random() * textures.length)];
 
         this.render = () => {
-            texture.getRoof().drawTransformedAt(0, 0, tRoof);
-            texture.getRoof().drawTransformedAt(0, -16, tRoof);
+            //texture.getRoof().drawTransformedAt(0, 0, tRoof);
+            /*
+            for(let i = 0; i < backWalls.length; ++i)
+                texture.getSide(backWalls[i]).drawTransformedAt(0, 0, tWalls[backWalls[i]]);
+            */
+            for(let i = 0; i < frontWalls.length; ++i)
+                texture.getSide(frontWalls[i]).drawTransformedAt(0, 0, tWalls[frontWalls[i]]);
+
+            texture.getRoof().drawTransformedAt(0, -32 * scale, tRoof);
         };
     };
     
     const setTransforms = (angle, pitch) => {
+        const radius = Math.sqrt(2) * 16;
+        
         tRoof.identity();
-        tRoof.scale(1, 0.5);
+        tRoof.scale(1, scale);
         tRoof.rotate(angle);
+
+        for(let i = 0; i < 4; ++i) {
+            const a = Math.PI * 0.5 * (i + 0.5) + angle;
+
+            tWalls[i].identity();
+            tWalls[i].scale(1, scale);
+            tWalls[i].translate(Math.cos(-a) * radius, Math.sin(-a) * radius);
+            tWalls[i].scale(-Math.cos(a - Math.PI * 0.25), 1);
+            tWalls[i].shear(0, -Math.cos(a + Math.PI * 0.25));
+        }
+
+        backWalls[0] = 3 - Math.floor(angle / (Math.PI * 0.5));
+        backWalls[1] = (backWalls[0] + 1) % 4;
+        frontWalls[0] = (backWalls[1] + 1) % 4;
+        frontWalls[1] = (frontWalls[0] + 1) % 4;
+
+        scale = Math.cos(pitch);
     };
 
     const update = timeStep => {
         angle += timeStep;
 
-        if(angle > Math.PI * 2)
+        while(angle > Math.PI * 2)
             angle -= Math.PI * 2;
 
-        setTransforms(angle, 0.5);
+        setTransforms(angle, pitch);
     };
 
     const render = () => {
@@ -34,7 +60,7 @@ const Isometric = function(myr) {
         myr.push();
 
         myr.translate(myr.getWidth() / 2, myr.getHeight() / 2);
-        myr.scale(2, 2);
+        myr.scale(6, 6);
 
         for(let i = 0; i < blocks.length; ++i)
             blocks[i].render();
@@ -60,26 +86,30 @@ const Isometric = function(myr) {
 
     const sprites = new myr.Surface("img/blocks.png", 128, 32);
     myr.register("roof", myr.makeSpriteFrame(sprites, 0, 0, 32, 32, 16, 16, 0));
-    myr.register("wall1", myr.makeSpriteFrame(sprites, 32, 0, 32, 32, 0, 0, 0));
-    myr.register("wall2", myr.makeSpriteFrame(sprites, 64, 0, 32, 32, 0, 0, 0));
-    myr.register("wall3", myr.makeSpriteFrame(sprites, 96, 0, 32, 32, 0, 0, 0));
+    myr.register("wall1", myr.makeSpriteFrame(sprites, 32, 0, 32, 32, 0, 32, 0));
+    myr.register("wall2", myr.makeSpriteFrame(sprites, 64, 0, 32, 32, 0, 32, 0));
+    myr.register("wall3", myr.makeSpriteFrame(sprites, 96, 0, 32, 32, 0, 32, 0));
 
     const CLEAR_COLOR = new myr.Color(0.2, 0.8, 0.5);
     const tRoof = new myr.Transform();
     const tWalls = [new myr.Transform(), new myr.Transform(), new myr.Transform(), new myr.Transform()];
     const blocks = [];
+    const backWalls = [0, 1];
+    const frontWalls = [2, 3];
     const textures = [
         new Texture(
             new myr.Sprite("roof"),
             [
-                new Texture(new myr.Sprite("wall1")),
-                new Texture(new myr.Sprite("wall2")),
-                new Texture(new myr.Sprite("wall3"))
+                new myr.Sprite("wall1"),
+                new myr.Sprite("wall2"),
+                new myr.Sprite("wall3")
             ]
         )
     ];
 
     let angle = Math.PI / 4;
+    let pitch = Math.PI / 4;
+    let scale;
 
     myr.setClearColor(CLEAR_COLOR);
 };
