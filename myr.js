64b4b6ea57f6d39850fa51d76d17785101e07499
jobtalle <jobtalle@hotmail.com>
2019-01-21 19:19:29 +0100

    Removed faulty drawLine fix

--- myr.js
+++ myr.js
@@ -536,16 +536,6 @@ const Myr = function(canvasElement) {
     };
 
     this.primitives.drawLine = (color, x1, y1, x2, y2) => {
-        if (x1 > x2)
-            ++x1;
-        else
-            ++x2;
-
-        if (y1 > y2)
-            ++y1;
-        else
-            ++y2;
-
         pushVertexColor(RENDER_MODE_LINES, color, x1, y1);
         pushVertexColor(RENDER_MODE_LINES, color, x2 + 1, y2 + 1);
     };
